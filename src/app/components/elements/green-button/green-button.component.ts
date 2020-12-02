c8b189b28ac21a077b8f5506d36c4e9d410d8933
tomerOko <tomeroko2@gmail.com>
2020-12-02 11:33:15 +0200

    just ui

--- src/app/components/elements/green-button/green-button.component.ts
+++ src/app/components/elements/green-button/green-button.component.ts
@@ -7,21 +7,33 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class GreenButtonComponent implements OnInit {
 
-  @Input() isWide:boolean = true;
-  @Input() title:string;
-  @Input() is_router_ling:boolean;
-  @Input() link_to:string = "";
+  @Input() isWide; //wide button background or the thiner one
+  @Input() title:string; //the text in the button
+  @Input() is_router_link:number = 0; //0 or smaller to regular link <> 1 or greater to routner link
+  @Input() link_to:string = ""; // href/
 
+  is_href=[]
+  is_router=[]
   link_to_img="../../../../assets/big_greeny.png"
 
-  constructor() { }
+  constructor() {
+   }
 
   ngOnInit(): void {
-    console.log(this.title)
+    console.log(this.isWide+"--"+this.title+"--"+this.is_router_link+"--"+this.link_to)
     console.log(this.isWide)
-    if(this.isWide!=true){
+    if(this.isWide==false){
+      console.log("thin")
       this.link_to_img="../../../../assets/small_greeny.png"
     }
+    console.log(this.is_router_link)
+    if(this.is_router_link>0){
+      this.is_router.push(1)
+    }else{
+      this.is_href.push(1)
+    }
+    console.log(this.is_href)
+    console.log(this.is_router)
   }
 
 }
--- src/app/components/pages/hope-we-helped/hope-we-helped.component.ts
+++ src/app/components/pages/hope-we-helped/hope-we-helped.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HopeWeHelpedComponent implements OnInit {
 
+  hallow="number"
   constructor() { }
 
   ngOnInit(): void {
